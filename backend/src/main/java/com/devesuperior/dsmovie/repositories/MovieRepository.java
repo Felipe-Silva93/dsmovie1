ea8497e7505045649191fedf7cddb316974de30b
Felipe-Silva93 <felipesilva4127@gmail.com>
2022-03-13 10:03:42 -0300

    find movies

---
+++ backend/src/main/java/com/devesuperior/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.devesuperior.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devesuperior.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+	
+	
+}
