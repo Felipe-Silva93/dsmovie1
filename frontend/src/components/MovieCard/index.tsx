c782237758a42cdc350db42f47d71b6d2cf207e8
Felipe-Silva93 <felipesilva4127@gmail.com>
2022-03-13 20:42:02 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -7,28 +7,25 @@ type Props ={
 }
 
 
-function MovieCard({movie}: Props) {
+    function MovieCard({movie}: Props) {
 
+   
+
+        return(
+            <div>
+                <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+                <div className="dsmovie-card-bottom-container">
+                    <h3>{movie.title}</h3>
+                    <MovieScore count={movie.count} score={movie.score}/>
     
-
-
-    return (
-        <div>
-            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-            <div className="dsmovie-card-bottom-container">
-                <h3>{movie.title}</h3>
-                <MovieScore />
-                
-                <Link to={`/form/${movie.id}`}>
-
-                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-
-                </Link>
-
-
-
-            </div>
-        </div >
-    );
-}
-export default MovieCard;
+                    <Link to={`/form/${movie.id}`}> 
+    
+                        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+    
+                    </Link>
+                    
+                </div>
+            </div >
+        );
+    }
+    export default MovieCard;
